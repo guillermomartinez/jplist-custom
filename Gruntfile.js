6ec56998da605e69dca5feecbd0e626f2590d0c9
Guillermo <guillermomartinez@outlook.com>
2015-09-17 19:49:57 -0500

    add filter select

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,8 @@ module.exports = function(grunt) {
           'css/jplist.checkbox-dropdown.min.css',
           'css/jplist.pagination-bundle.min.css',
           'css/jplist.textbox-filter.min.css',
+          'css/jplist.filter-toggle-bundle.min.css',
+          'css/jplist.radio-buttons-dropdown.min.css',
         ],
         dest: 'css/jplist.custom.min.css'
       }    
@@ -29,7 +31,9 @@ module.exports = function(grunt) {
           'js/jplist.checkbox-dropdown.min.js',
           'js/jplist.pagination-bundle.min.js',
           'js/jplist.textbox-filter.min.js',
-          'js/jplist.sort-bundle.min.js'
+          'js/jplist.sort-bundle.min.js',
+          'js/jplist.filter-toggle-bundle.min.js',
+          'js/jplist.filter-dropdown-bundle.min.js',
         ],
         dest: 'js/jplist.custom.min.js'
       }      
