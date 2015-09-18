2cd7cf7b14e6ff648adab55043025764dd70b66b
Guillermo <guillermomartinez@outlook.com>
2015-09-17 19:07:57 -0500

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),   
+    cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        advanced: false
+      },
+      css_min: {
+        src: [
+          'core/jplist.core.min.css',
+          'css/jplist.checkbox-dropdown.min.css',
+          'css/jplist.pagination-bundle.min.css',
+          'css/jplist.textbox-filter.min.css',
+        ],
+        dest: 'css/jplist.custom.min.css'
+      }    
+    },
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      js_min: {
+        src: [
+          'core/jplist.core.min.js',
+          'js/jplist.checkbox-dropdown.min.js',
+          'js/jplist.pagination-bundle.min.js',
+          'js/jplist.textbox-filter.min.js',
+          'js/jplist.sort-bundle.min.js'
+        ],
+        dest: 'js/jplist.custom.min.js'
+      }      
+    }
+  });
+  
+  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+  require('time-grunt')(grunt);
+  
+  grunt.registerTask('dist', ['cssmin:css_min','uglify:js_min']);
+
+
+};
